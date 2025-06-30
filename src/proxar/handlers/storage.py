ab6a56f5876c4f0138cce454f99c7d7fc57b1065
Filming <79931565+filming@users.noreply.github.com>
2025-06-30 11:29:37 -0400

    refactor: improve structure of main class init

--- src/proxar/handlers/storage.py
+++ src/proxar/handlers/storage.py
@@ -9,20 +9,26 @@ logger = logging.getLogger(__name__)
 class StorageHandler:
     """Handle persistent, asynchronous storage of proxies in a JSON file."""
 
-    def __init__(self, storage_dir: str | Path | None):
-        """Initialize the storage handler instance.
+    def __init__(self, storage_dir: str | Path | None) -> None:
+        """Initializes the StorageHandler instance.
 
         Args:
-            storage_dir (str | Path | None): The path to store proxy
-                files. If None, a default directory is used based on
-                the operating system.
+            storage_dir: The directory path to store the proxies.json file. If None,
+                an OS-specific user data directory is used.
         """
-        # Set storage path and ensure its existence
+        # Set a flag to track unsaved proxies.
+        self._dirty: bool = False
+
+        # Set storage dir path and ensure its existence
         if storage_dir is None:
-            self.storage_path = Path(platformdirs.user_data_dir("proxar"))
+            self.storage_dir_path = Path(platformdirs.user_data_dir("proxar"))
+            logger.debug(
+                "No storage directory provided. Using default path: %s",
+                self.storage_dir_path,
+            )
         else:
-            self.storage_path = Path(storage_dir)
+            self.storage_dir_path = Path(storage_dir)
+            logger.debug("Using provided storage directory: %s", self.storage_dir_path)
 
-        self.storage_path.mkdir(parents=True, exist_ok=True)
-
-        logger.debug("Storage handler has been initialized.")
+        self.storage_dir_path.mkdir(parents=True, exist_ok=True)
+        self.json_file_path = self.storage_dir_path / "proxies.json"
